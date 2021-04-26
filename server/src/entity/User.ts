fd85179ca1147d5890371ebf89a92a92989877b7
aman tiwari <amantiwari78632@gmail.com>
2021-04-26 23:01:05 +0530

    created user model orm

---
+++ server/src/entity/User.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@ObjectType()
+@Entity("users")
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  @Field()
+  id: number;
+  @Field()
+  @Column("text")
+  email: string;
+
+  @Column("text")
+  password: string;
+
+  @Column("int", { default: 0 })
+  tokenVersion: number;
+}

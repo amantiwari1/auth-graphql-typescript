04757cbe597e4677dd5ab79613d054a69a80e4d3
aman tiwari <amantiwari78632@gmail.com>
2021-04-26 22:58:44 +0530

    created server

---
+++ server/src/index.ts
@@ -0,0 +1,75 @@
+import express from "express";
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
+import { createConnection } from "typeorm";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken, createRefreshToken } from "./auth";
+import { sendRequestToken } from "./sendRefreshToken";
+
+(async () => {
+  const app = express();
+  app.use(cookieParser());
+  app.get("/", (_, res) => {
+    return res.send("hello");
+  });
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+
+    if (!token) {
+      return res.json({ ok: false, accessToken: "" });
+    }
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (e) {
+      console.log(e);
+      return res.json({ ok: false, accessToken: "" });
+    }
+    const user = await User.findOne({ id: payload.userId });
+
+    if (!user) {
+      return res.json({ ok: false, accessToken: "" });
+    }
+
+    if (user.tokenVersion !== payload.tokenVersion) {
+      return res.json({ ok: false, accessToken: "" });
+    }
+    sendRequestToken(res, createRefreshToken(user));
+    return res.json({ ok: true, accessToken: createAccessToken(user) });
+  });
+  await createConnection();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [UserResolver] }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("====================================");
+    console.log("http://localhost:4000");
+    console.log("====================================");
+  });
+})();
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
